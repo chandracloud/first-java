930ad742250c8df57dc8aeb75e701358f043a6e7
unknown <chandracloud3soft@gmail.com>
2023-09-03 14:52:09 +0530

    added helloworld

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,5 @@
-// first program
-
 class Helloworld{
     public static void main(String[] args) {
-        System.out.println("Hello world");        
+        System.out.println("Hello world");
     }
 }
