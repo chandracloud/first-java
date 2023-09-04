ff5af34faa05af829d16285008de573b0e0c20a2
root <root@instance-1.asia-south1-c.c.inspiring-tower-396215.internal>
2023-09-04 06:00:05 +0000

    branches

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 class Helloworld{
     public static void main(String[] args) {
         System.out.println("Hello world");
-	System.out.println("Welcome clones");
+	System.out.println("Welcome branching");
 	
     }
 }
